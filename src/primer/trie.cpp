ca3980dec9cf23911ad8407e0a01ed438d7d41f5
luluwalulu <553694212@qq.com>
2026-01-15 16:31:36 +0800

    Put函数完成

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -10,6 +10,7 @@ auto Trie::Get(std::string_view key) const -> const T * {
   // nullptr. After you find the node, you should use `dynamic_cast` to cast it to `const TrieNodeWithValue<T> *`. If
   // dynamic_cast returns `nullptr`, it means the type of the value is mismatched, and you should return nullptr.
   // Otherwise, return the value.
+  if(root_==nullptr) return nullptr;
   std::shared_ptr<const TrieNode> p(root_);
   for(int i=0;i<key.size();i++){
     if(p->children_.find(key[i])==p->children_.end()) return nullptr;
@@ -22,46 +23,38 @@ auto Trie::Get(std::string_view key) const -> const T * {
 }
 
 template <class T>
-auto Trie::put_reversal(std::shared_ptr<TrieNode> p,int index,std::string_view key,T val) const -> std::shared_ptr<const TrieNode> {
+auto Trie::put_reversal(std::shared_ptr<TrieNode> p,int index,std::string_view key,T value) const -> std::shared_ptr<const TrieNode> {
   // 插入完成的终止条件
   if(index==key.size()){
-
+    shared_ptr<T> value=make_shared<T>(std::move(value));
+    return std::make_shared<TrieNodeWithValue>(value);
   }
   std::shared_ptr<TrieNode> next;
   // 如果p的孩子中存在序号为index的对应节点
   if(p->children_.find(key[index])!=p->children_.end())
     next=std::shared_ptr<TrieNode>(std::move(p->children_[key[index]]->Clone()));
   else
-    next=std::make_shared<TrieNode>(std::map<char, std::shared_ptr<const TrieNode>>());
+    next=std::make_shared<TrieNode>();
 
   p->children_[key[index]]=put_reversal(next,index+1,key,val);
 
   return p; 
 }
 
+// 路径上的节点，要么拷贝，要么新建（原Trie该节点为空），不能修改原本的Trie的结构
 template <class T>
 auto Trie::Put(std::string_view key, T value) const -> Trie {
   // Note that `T` might be a non-copyable type. Always use `std::move` when creating `shared_ptr` on that value.
 
   // You should walk through the trie and create new nodes if necessary. If the node corresponding to the key already
   // exists, you should create a new `TrieNodeWithValue`.
-  std::shared_ptr<TrieNode> newroot,p;
-  newroot=std::shared_ptr<TrieNode>(std::move(root_->Clone()));
-  p=newroot;
-  for(int i=0;i<key.size()-1;i++){
-    std::shared_ptr<TrieNode> temp;
-    if(p->children_.find(key[i])==p->children_.end()){
-      temp=std::make_shared<TrieNode>(std::map<char, std::shared_ptr<const TrieNode>>());
-      p->children_[key[i]]=std::shared_ptr<const TrieNode>(temp);
-    }
-    else{
-      temp=std::shared_ptr<TrieNode>(std::move(p->children_[key[i]]->Clone()));
-      p->children_[key[i]]=std::shared_ptr<const TrieNode>(temp);
-    }
-    p=temp;
-  }
-
-
+  std::shared_ptr<TrieNode> newroot;
+  if(!root_)
+    newroot=std::make_shared<TrieNode>();
+  else
+    newroot=std::shared_ptr<TrieNode>(std::move(root_->Clone()));
+  put_reversal(newroot,0,key,std::move(value));
+  return Trie(newroot);
 }
 
 auto Trie::Remove(std::string_view key) const -> Trie {
