f9bf19327455c277949b9867f721912405059163
luluwalulu <553694212@qq.com>
2026-01-24 17:50:25 +0800

    Task1超级简单测试通过

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -37,30 +37,61 @@ auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard
   return *this;
 }
 
-BasicPageGuard::~BasicPageGuard(){
-  if (!page_) {
-    return;
-  }
-  bpm_->UnpinPage(PageId(), is_dirty_);
+BasicPageGuard::~BasicPageGuard() { Drop(); };  // NOLINT
+
+ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept : guard_(std::move(that.guard_)) {  }
+
+auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & {
+  Drop();
+
+  this->guard_ = std::move(that.guard_);
+
+  return *this;
+}
+
+void ReadPageGuard::Drop() {
+  guard_.page_->RUnlatch();
+  guard_.Drop();
+}
+
+ReadPageGuard::~ReadPageGuard() { Drop(); }  // NOLINT
+
+WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept : guard_(std::move(that.guard_)) {  }
+
+auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & {
+  Drop();
+
+  this->guard_ = std::move(that.guard_);
+
+  return *this;
+}
+
+void WritePageGuard::Drop() {
+  guard_.page_->WUnlatch();
+  guard_.Drop();
+}
+
+WritePageGuard::~WritePageGuard() { Drop(); }  // NOLINT
+
+auto BasicPageGuard::UpgradeRead() -> ReadPageGuard {
+  // 在升级期间页不能被驱逐，也就是pin_count始终不能为0
+  // 之后原本的页应该无效，只能使用最新的页
+
+  // 关键在于绝对不能调用unpin，因为资源自始至终只有一份，一旦中途调用了unpin，就意味着放弃了这份资源
+  // 同时也不要给旧的BasicPageGuard调用Unpin的机会
+  // 需要表现出当前BasicPageGuard资源已经被移空
+  ReadPageGuard rg(bpm_, page_);
   bpm_ = nullptr;
   page_ = nullptr;
-  is_dirty_ = false;
-};  // NOLINT
+  return rg;
+}
 
-ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default;
-
-auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & { return *this; }
-
-void ReadPageGuard::Drop() {}
-
-ReadPageGuard::~ReadPageGuard() {}  // NOLINT
-
-WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept = default;
-
-auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & { return *this; }
-
-void WritePageGuard::Drop() {}
-
-WritePageGuard::~WritePageGuard() {}  // NOLINT
+auto BasicPageGuard::UpgradeWrite() -> WritePageGuard {
+  WritePageGuard wg(bpm_, page_);
+  bpm_ = nullptr;
+  page_ = nullptr;
+  return wg;
+}
 
 }  // namespace bustub
+
