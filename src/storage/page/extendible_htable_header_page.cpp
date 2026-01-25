5b50f166435e8c919a1a48830a90e9a08f2226f6
luluwalulu <553694212@qq.com>
2026-01-25 09:43:06 +0800

    Task2:Header完成

--- src/storage/page/extendible_htable_header_page.cpp
+++ src/storage/page/extendible_htable_header_page.cpp
@@ -17,17 +17,21 @@
 namespace bustub {
 
 void ExtendibleHTableHeaderPage::Init(uint32_t max_depth) {
-  throw NotImplementedException("ExtendibleHTableHeaderPage is not implemented");
+  max_depth_ = max_depth;
 }
 
-auto ExtendibleHTableHeaderPage::HashToDirectoryIndex(uint32_t hash) const -> uint32_t { return 0; }
+auto ExtendibleHTableHeaderPage::HashToDirectoryIndex(uint32_t hash) const -> uint32_t {
+  return hash>>30;
+}
 
-auto ExtendibleHTableHeaderPage::GetDirectoryPageId(uint32_t directory_idx) const -> uint32_t { return 0; }
+auto ExtendibleHTableHeaderPage::GetDirectoryPageId(uint32_t directory_idx) const -> uint32_t {
+  return directory_page_ids_[directory_idx];
+}
 
 void ExtendibleHTableHeaderPage::SetDirectoryPageId(uint32_t directory_idx, page_id_t directory_page_id) {
-  throw NotImplementedException("ExtendibleHTableHeaderPage is not implemented");
+  directory_page_ids_[directory_idx] = directory_page_id;
 }
 
-auto ExtendibleHTableHeaderPage::MaxSize() const -> uint32_t { return 0; }
+auto ExtendibleHTableHeaderPage::MaxSize() const -> uint32_t { return max_depth_; }
 
 }  // namespace bustub
