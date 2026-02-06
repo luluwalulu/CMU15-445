52e9c5aaca399cc8737a1cb570ef929ce3502c4c
luluwalulu <553694212@qq.com>
2026-02-06 10:06:36 +0800

    Task1:seq_scan_executor FINISH

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,39 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {
+  Init();
+}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+  // 根据ExecutorContext初始化SeqScanPlanNode
+  // table_oid_t oid = plan_->GetTableOid();
+  // itr_ = exec_ctx_->GetCatalog()->GetTable()
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  auto catalog = exec_ctx_->GetCatalog();
+  table_oid_t oid = plan_->GetTableOid();
+  auto table_info = catalog->GetTable(oid);
+  itr_.emplace(table_info->table_->MakeIterator());
+}
+
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  // 不需要从child_executor那里获取什么，直接根据itr_得到下一个应该返回的tuple
+  if(!itr_.has_value()) {
+    std::cout<<"在执行SeqScanExecutor::Next之前本应进行初始化"<<std::endl;
+    return false;
+  }
+
+  while(!itr_->IsEnd()){
+    auto pii = itr_->GetTuple();
+    if(pii.first.is_deleted_) {
+      ++*itr_;
+      continue;
+    }
+    *tuple = pii.second;
+    return true;  
+  }
+
+  return false;
+}
 
 }  // namespace bustub
