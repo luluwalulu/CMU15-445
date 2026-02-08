ef1c12b20facb6ccf080a6ea690b668159b14806
luluwalulu <553694212@qq.com>
2026-02-08 09:55:07 +0800

    index_scan_executor FINISH

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -13,10 +13,51 @@
 
 namespace bustub {
 IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void IndexScanExecutor::Init() { throw NotImplementedException("IndexScanExecutor is not implemented"); }
+void IndexScanExecutor::Init() {
+  std::vector<Value> values;
+  Tuple dummy_tuple;
+  Value v;
 
-auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  auto catalog =exec_ctx_->GetCatalog();
+  auto table = catalog->GetTable(plan_->table_oid_);
+  auto index = catalog->GetIndex(plan_->index_oid_);
+  auto htable = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(index->index_.get());
+  const auto& schema = table->schema_;
+  const auto& key_schema = index->key_schema_;
+
+  if(plan_->pred_key_) {
+    v = plan_->pred_key_->Evaluate(&dummy_tuple, Schema{{}});
+  } else {
+    const auto& expr = plan_->filter_predicate_;
+    v = expr->Evaluate(&dummy_tuple, Schema{{}});
+  }
+
+  values.push_back(v);
+  Tuple key(values, &key_schema);
+
+  htable->ScanKey(key, &rids_, exec_ctx_->GetTransaction());
+}
+
+auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  auto catalog = exec_ctx_->GetCatalog();
+  auto table_info = catalog->GetTable(plan_->table_oid_);
+  auto table_heap = table_info->table_.get();
+
+  while(cursor_<rids_.size()) {
+    auto p = table_heap->GetTuple(rids_[cursor_]);
+    if(p.first.is_deleted_) {
+      cursor_++;
+      continue;
+    }
+
+    *tuple = p.second;
+    *rid = rids_[cursor_];
+    return true;
+  }
+
+  return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/index_scan_executor.h
+++ src/include/execution/executors/index_scan_executor.h
@@ -44,5 +44,7 @@ class IndexScanExecutor : public AbstractExecutor {
  private:
   /** The index scan plan node to be executed. */
   const IndexScanPlanNode *plan_;
+  std::vector<RID> rids_;
+  size_t cursor_{0};
 };
 }  // namespace bustub
